77a6b1d7e2de88696a5801ca54a317251d5b973a
0xkabir <kabir.sadiken1@gmail.com>
2022-10-12 03:46:54 +0600

    added toast on clicking the quiz options and made the quizes responsive

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -3,17 +3,23 @@ import { useLoaderData } from 'react-router-dom';
 import QuizQuestion from '../QuizQuestion/QuizQuestion';
 
 const Quiz = () => {
-    const {name,questions} = useLoaderData().data
+    const {name,total, questions} = useLoaderData().data
     let serial = 0
     return (
         <div>
-            <h2 className='text-4xl font-medium text-center'>Quiz of {name}</h2>
+            <div className='pt-10'>
+                <h2 className='text-3xl font-medium text-center'>Quiz of {name}</h2>
+                <p className='text-center'>{total} Quizes | 10 Minutes</p>
+            </div>
             {
                 questions.map(question => {
                     serial++;
                     return <QuizQuestion key={question.id} quizQuestion={question} serial={serial}></QuizQuestion>
                 })
             }
+            <div className='text-center'>
+                <button className='bg-slate-800 hover:bg-slate-900 text-white font-medium mt-5 mb-10 px-5 py-2 rounded-lg'>Finish Quiz</button>
+            </div>
         </div>
     );
 };
--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -1,26 +1,65 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Markup } from 'interweave';
 import './QuizQuestion.css'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const QuizQuestion = ({serial, quizQuestion}) => {
     const {question, options, correctAnswer} = quizQuestion
-    const [selected, setSelected] = useState()
-    console.log(selected)
+    const showToast = answer => {
+        if(answer === correctAnswer){
+            return toast.success('Correct Answer!!', {
+                position: "bottom-right",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+        }
+        else{
+            return toast.error('Incorrect Answer', {
+                position: "bottom-right",
+                autoClose: 2500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "dark",
+                });
+        }
+
+    }
     
     return (
-        <div className='w-[400px] mx-auto bg-gray-200 p-10 my-5 rounded-xl shadow-lg'>
+        <div className='w-[340px] md:w-[400px] mx-auto bg-gray-200 p-5 md:p-10 my-5 rounded-xl shadow-lg'>
             <div className='flex items-start question'>
                 {serial}. <Markup content={question}/>
             </div>   
             <div>
                 {
-                    options.map((option, index) => <div key={index} className='hover:bg-slate-300 rounded' onClick={()=>setSelected(option)}>
-                        <input type="radio" name={`question-${serial}`} id={`option-${serial}-${index}`} />
-                        <label htmlFor={`option-${serial}-${index}`}>{option}</label>
-                    </div>)
-                }
+                    options.map((option, index) =><div key={index} className='hover:bg-slate-300 rounded'>
+                            <input type="radio" name={`question-${serial}`} id={`option-${serial}-${index}`} onClick={()=>showToast(option)}/>
+                            <label htmlFor={`option-${serial}-${index}`}>{option}</label>
+                            <ToastContainer
+                            position="bottom-right"
+                            autoClose={2500}
+                            hideProgressBar={false}
+                            newestOnTop={false}
+                            closeOnClick
+                            rtl={false}
+                            pauseOnFocusLoss
+                            draggable
+                            pauseOnHover
+                            theme="dark"
+                            />
+                        </div>)
+                    }
+                </div>
             </div>
-        </div>
     );
 };
 

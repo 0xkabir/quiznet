e78fb9bb0f5527edd8a9485791b392570b101690
0xkabir <kabir.sadiken1@gmail.com>
2022-10-12 09:52:09 +0600

    displayed the correct answer on clicking the eye-icon

--- src/components/QuizQuestion/QuizQuestion.js
+++ src/components/QuizQuestion/QuizQuestion.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Markup } from 'interweave';
 import './QuizQuestion.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 
 const QuizQuestion = ({serial, quizQuestion}) => {
     const {question, options, correctAnswer} = quizQuestion
@@ -33,9 +35,22 @@ const QuizQuestion = ({serial, quizQuestion}) => {
         }
 
     }
+
+    const [show, setShow] = useState(false)
     
     return (
-        <div className='w-[340px] md:w-[400px] mx-auto bg-gray-200 p-5 md:p-10 my-5 rounded-xl shadow-lg'>
+        <div className={'w-[340px] md:w-[400px] mx-auto bg-gray-200 p-5 md:p-10 my-5 rounded-xl shadow-lg'}>
+            <div className={'flex flex-col justify-center relative'}>
+                <div onClick={()=>setShow(!show)} className='cursor-pointer absolute top-0 md:top-[-20px] right-0 md:right-[-10px]'>
+                    {
+                        show?<FontAwesomeIcon icon={faEyeSlash}></FontAwesomeIcon>:<FontAwesomeIcon icon={faEye}></FontAwesomeIcon>
+                    }
+                </div>
+            <div className={show?'block':'hidden'}>
+                <h2 className='text-2xl font-medium'>Correct Answer:</h2>
+                <p className='text-center pt-5'>{correctAnswer}</p>
+            </div>
+            <div className={show?'hidden':'block'}>
             <div className='flex items-start question'>
                 {serial}. <Markup content={question}/>
             </div>   
@@ -60,6 +75,8 @@ const QuizQuestion = ({serial, quizQuestion}) => {
                     }
                 </div>
             </div>
+            </div>    
+        </div>
     );
 };
 
